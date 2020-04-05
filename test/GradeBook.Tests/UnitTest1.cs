e7a84069388c57e6c886e8d5da9ce30d4954f019
arindamdawn <arindamdawn3@gmail.com>
2020-04-05 13:00:58 +0530

    Added gitignore

---
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+            var x = 5;
+            var y = 1;
+            var expected = 6;
+            //act
+            var sum = x + y;
+            //assert
+            Assert.Equal(expected, sum);
+        }
+    }
+}
